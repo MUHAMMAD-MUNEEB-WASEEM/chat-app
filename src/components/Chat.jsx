34134a3c9dfdbc56f3cf298903beebd9d4bc6c54
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2022-09-13 13:16:48 +0500

    create chatsection top bar

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Cam from "../img/cam.png";
+import Add from "../img/add.png";
+import More from "../img/more.png";
+import Messages from './Messages';
+import Input from './Input';
 
 function Chat() {
   return (
-    <div className='chat'>Chat</div>
+    <div className='chat'>
+      <div className="chatInfo">
+        
+        <span>Muneeb</span>
+        <div className="chatIcons">
+          <img src={Cam} alt="" />
+          <img src={Add} alt="" />
+          <img src={More} alt="" />
+        </div>
+
+      </div>
+      <Messages />
+      <Input/>
+    </div>
   )
 }
 
